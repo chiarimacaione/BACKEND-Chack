92ff6b5c1fb48c4e8a2d04cd10a2171450c476ef
Chiara Macaione <chiarimacaione@gmail.com>
2025-02-12 23:54:30 +0100

    img size

--- src/config/multer.config.js
+++ src/config/multer.config.js
@@ -23,6 +23,6 @@ const storage = new CloudinaryStorage({
     },
 });
 
-const upload = multer({ storage });
+const upload = multer({ storage, limits: { fileSize: 1024 * 1024 * 5 } });
 
 export default upload;
