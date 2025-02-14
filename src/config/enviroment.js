be7793a8fbd3fb7bcb114de6186c8d9d81e19ed6
Chiara Macaione <chiarimacaione@gmail.com>
2025-02-14 23:31:54 +0100

    enviroment

--- src/config/enviroment.js
+++ src/config/enviroment.js
@@ -16,7 +16,8 @@ const ENVIROMENT = {
         DB_USER: process.env.DB_USER,
         DB_PASSWORD: process.env.DB_PASSWORD,
         DB_NAME: process.env.DB_NAME,
-        DB_PORT: process.env.DB_PORT
+        DB_PORT: process.env.DB_PORT,
+        DB_URI: process.env.DB_URI
     },
     CLOUDINARY: {
         CLD_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
